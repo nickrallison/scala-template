99dfcdefa036fedb131159230a4b123bef5a1863
nickrallison <nickrallison1@gmail.com>
2024-09-04 09:39:22 -0600

    init

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+    description = "A flake for chisel & verilator development";
+    inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    outputs = { self, nixpkgs }: {
+
+        # devShell.x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux; mkShellNoCC {
+        #   packages = with pkgs; [
+        #     verilator
+        #     ninja
+        #     cmake
+        #     clang_18
+        #   ];
+        # };
+        devShell.aarch64-darwin = with nixpkgs.legacyPackages.aarch64-darwin; mkShellNoCC {
+            packages = with pkgs; [
+                # Scala 
+                sbt
+
+                # Java
+                zulu8
+
+                # Verilator
+                verilator
+                ninja
+                cmake
+            ];
+            shellHook = ''
+                export CXX=/usr/bin/c++
+            '';
+        };
+    };
+}
